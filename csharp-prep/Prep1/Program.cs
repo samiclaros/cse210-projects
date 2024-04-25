f563030673a0b965c364e5791ee7f8286043dcff
samiclaros <sclarosgarcia@gmail.com>
2024-04-25 19:42:11 -0400

    First pracrice done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
-using System;
+// using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep1 World!");
+//     }
+// }
+
+Console.Write("What is your fisrt name? ");
+Console.Write("");
+string first_name = Console.ReadLine();
+Console.Write("What is your last name? ");
+Console.Write("");
+string last_name = Console.ReadLine();
+Console.Write("");
+Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
