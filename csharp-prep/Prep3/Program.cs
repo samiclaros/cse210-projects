797b89dd3fd0d67ae5eb5f8c0b4a0a700d46a8e8
samiclaros <sclarosgarcia@gmail.com>
2024-04-26 17:48:50 -0400

    Until step 2, I had an error with the debugger

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magin number?: ");
+        string number = Console.ReadLine();
+        int numberConversion = int.Parse(number);
+
+        string keyWord = "yes";
+
+        while (keyWord == "yes")
+        Console.Write("What is your guess?: ");
+        string guess = Console.ReadLine();
+        int guessConversion = int.Parse(number);
+
+        if (guessConversion == numberConversion)
+        {
+            Console.Write(""); //BLANK
+            Console.Write("You guessed it");
+        }
+        else if (guessConversion > numberConversion)
+        {
+            Console.Write(""); //BLANK
+            Console.Write("Lower");
+        }
+        else
+        {
+            Console.Write(""); //BLANK
+            Console.Write("Higher");
+        }
     }
 }
