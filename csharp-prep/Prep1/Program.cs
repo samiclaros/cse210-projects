50d341134baf9834a54ccf94ce52605c9951c61b
samiclaros <sclarosgarcia@gmail.com>
2024-04-26 17:00:01 -0400

    Fixed Prep 1 and finished Prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,18 +1,17 @@
-// using System;
+using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine("Hello Prep1 World!");
-//     }
-// }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your fisrt name? ");
+        Console.Write("");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        Console.Write("");
+        string last_name = Console.ReadLine();
+        Console.Write("");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+    }
+}
 
-Console.Write("What is your fisrt name? ");
-Console.Write("");
-string first_name = Console.ReadLine();
-Console.Write("What is your last name? ");
-Console.Write("");
-string last_name = Console.ReadLine();
-Console.Write("");
-Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percentageNumber = int.Parse(percentage);
+
+        string letter = "";
+
+        if (percentageNumber >= 80)
+        {
+            letter = "A";
+        }
+        else if (percentageNumber >= 70)
+        {
+            letter = "B";
+        }
+        else if (percentageNumber >= 60)
+        {
+            letter = "C";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percentageNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course");
+        }
+        else
+        {
+            Console.WriteLine("You have to do more for the next semester!");
+        }
     }
 }
