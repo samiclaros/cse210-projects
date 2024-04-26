e21b05aa2581d1b3bbbd2873bd6771f61ec84da7
samiclaros <sclarosgarcia@gmail.com>
2024-04-26 19:10:03 -0400

    Stretch challenges almost completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,39 +1,38 @@
 using System;
-using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Console.Write("What is the magic number?: ");
-        // string number = Console.ReadLine();
-        // int numberConversion = int.Parse(number);
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,200);
-        int proTip = 0;
+        int number = randomGenerator.Next(1, 200);
+        string keyWord = "yes";
+        int counter = 0;
 
-        while (proTip == 0)
+        while (keyWord == "yes")
         {
+            counter = counter + 1;
             Console.Write("What is your guess?: ");
             string guess = Console.ReadLine();
             int guessConversion = int.Parse(guess);
 
             if (guessConversion == number)
             {
-                Console.WriteLine("You guessed it");
-                Console.Write(""); //BLANK
-                break;
+                Console.WriteLine($"You guessed it in {counter} times.");
+                Console.Write("Do you want to play again? (yes/no): ");
+                keyWord = Console.ReadLine();
             }
             else if (guessConversion > number)
             {
                 Console.WriteLine("Lower");
-                Console.Write(""); //BLANK
             }
             else if (guessConversion < number)
             {
                 Console.WriteLine("Higher");
-                Console.Write(""); //BLANK
             }
         }
     }
 }
+
+// I tried to do the loop with the "yes" but I can't, I don't know how to make the counter restart from 0 and
+// The "random" number in the second try, it's the same.. so Idk why
