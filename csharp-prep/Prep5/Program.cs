561e5c20c07ad97b90a51fc7d16746cf3ad80499
samiclaros <sclarosgarcia@gmail.com>
2024-04-27 21:45:22 -0400

    Prep 5 finished, I only checked out the main part because I get confused and added the DisplayFinal function too

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+            DisplayMessage();
+
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+
+            int squaredNumber = SquareNumber(userNumber);
+            
+            FinalDisplay(userName, squaredNumber);
     }
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string userNumber = Console.ReadLine();
+            int userNumberConversion = int.Parse(userNumber);
+            return userNumberConversion;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        
+        static void FinalDisplay(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+    
 }
