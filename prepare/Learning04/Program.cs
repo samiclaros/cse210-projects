3f9a525fc7d24ad308c5e74cacc3294ae4ba697c
samiclaros <sclarosgarcia@gmail.com>
2024-05-15 18:12:46 -0400

    Practice of Learning activities Finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment prove = new("Samuel Bennet", "Multiplication");
+        Console.WriteLine(prove.GetSumary());
+
+        MathAssignment math = new("Samuel Bennet", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // DateTime startTime = DateTime.Now;
+        // DateTime futureTime = startTime.AddSeconds(5);
+
+        // Thread.Sleep(3000);
+
+        // DateTime currentTime = DateTime.Now;
+        // if (currentTime < futureTime)
+        // {
+        //     Console.WriteLine("We have not arrived at our future time yet...");
+        // }
+
+        // Console.Write("+");
+
+        // Thread.Sleep(500);
+
+        // Console.Write("\b \b"); // Erase the + character
+        // Console.Write("-"); // Replace it with the - character
     }
 }
